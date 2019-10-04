3ecdb9d4ddaf4e6cbe220ae505e598d1e4c9f553
rijpma <auke.rijpma@gmail.com>
2019-10-04 11:45:29 +0200

    cleanup preflight

--- R/prep.R
+++ R/prep.R
@@ -2,8 +2,7 @@
 
 #' Check whether dataset is ready for linkage
 #' 
-#' \code{preflight} checks whether a dataset is ready for 
-#' linkage and prints the results.
+#' \code{preflight} checks whether a dataset is ready for linkage
 #' 
 #' Preflight does a number of checks on the dataset.
 #' \itemize{
@@ -16,16 +15,15 @@
 #'          choice to the user. Different cases do count for the string distance.
 #'      \item The share of accented letters and non-alphabetic symbols by variable. A
 #'          gain, these are not automatically fixed, but left to the user's discretion. 
-#'          Note that some analphabetics (for instance "." or "-") could be frequently and consistently applied in the data. String distance calculation does not fail on accented letters, but it does count towards the string distance.
+#'          Note that some analphabetics (for instance "." or "-") could be frequently 
+#'          and consistently applied in the data. String distance calculation does not 
+#'          fail on accented letters, but it does count towards the string distance.
 #' }
 #' 
 #' Currently, capelinker expects the dataset to be about couples. That is, data about 
 #' husband and spouse is used for linkage because together they contain far more 
 #' information than a single individual. The pretrained data is done on marriage and the 
 #' parents baptism registries.
-#' 
-#' Later versions will include models based on the opgaafrolle, but these too usually 
-#' provide husband and spouse names so linkage will work best on data with couples.
 #' 
 #' The following variables are expected by the functions and models:
 #' \itemize{
@@ -36,7 +34,11 @@
 #'      \item \code{minitils} male initials in the form JF (so no K, and no punctuation)
 #'      \item \code{winitils} female initials in the form JF (so no K, and no punctuation)
 #'      \item \code{year} the year of observation of the two records
-#'      \item \code{mprof} the male profession
+#' }
+#' 
+#' For the baptisms
+#' \itemize{
+#'  \item \code{mprof} the male profession
 #' }
 #' 
 #' For the opgaafrollen.
@@ -45,12 +47,15 @@
 #'  \item \code{wifeinboth}
 #'  \item \code{spousenamedist}
 #'  \item \code{namefreq}
+#'  \item \code{settlerchildrendist}
 #' }
+#' 
 #' 
 #' The following models can be used 
 #' \itemize{
 #'  \item \code{m_rf_baptisms_sparse} a model linking parents in baptism records to marriage records, based on minimal information: male surname (mlast), male first name (mfirst), female first name (wfirst, female surname not used because it would typically not be reported in the baptism records), and year of marriage/baptism (year).
 #'  \item \code{m_rf_baptisms_full} a model linking parents in baptism and marriage records, using additional information: initials, profession, and soundex distances of the names. Performance is not much better than the sparse model.
+#'  \item \code{opgaafrol_full} a model linking households from one year to the next
 #' }
 #' 
 #' @param dat the dataset to check, as a data.table.
@@ -68,40 +73,29 @@ preflight = function(dat,
 
     modstring = match.arg(modstring)
 
-    # pattern = "dist|sdx"
-    # data("pretrained_models")
-    # vrbs = sapply(pretrained_models, `[[`, "variables")
-    # lapply(vrbs, function(x) unique(gsub(pattern, "", x[grepl(pattern, x)])))
-    # vrbs = pretrained_models[[modstring]]$variables
-    # vrbs = gsub(pattern, "", vrbs[grepl(pattern, vrbs)])
+    pattern = "dist$|sdx$"
 
-    vrblist = list(
-        m_rf_baptisms_sparse = c("mlast", "mfirst", "wfirst", "year"),
-        m_rf_baptisms_full = c("mlast", "mfirst", "wfirst", "winitials", "minitials", "mprof", "year"),
-        opgaafrol_full = c("mlast", "mfirst", "wfirst", "wlast", "winitials", "minitials", "settlerchildren")
-    )
-    vrbs = vrblist[[modstring]]
+    data("pretrained_models")
 
-    # vrbs_baptisms_sparse = c("mlast", "mfirst", "wfirst", "year")
-    # vrbs_baptisms_full = c("mlast", "mfirst", "wfirst", "winitials", "minitials", "mprof", "year")
-    # vrbs_opgaafrol_full = c("mlast", "mfirst", "wfirst", "wlast", "winitials", "minitials", "settlerchildren")
+    vrbs = pretrained_models[[modstring]]$variables
+    vrbs = gsub(pattern, "", vrbs[grepl(pattern, vrbs)])
+
+    # vrblist = list(
+    #     m_rf_baptisms_sparse = c("mlast", "mfirst", "wfirst", "year"),
+    #     m_rf_baptisms_full = c("mlast", "mfirst", "wfirst", "winitials", "minitials", "mprof", "year"),
+    #     opgaafrol_full = c("mlast", "mfirst", "wfirst", "wlast", "winitials", "minitials", "settlerchildren")
+    # )
+    # vrbs = vrblist[[modstring]]
+
 
     vrbs_missing = setdiff(vrbs, names(dat))
-    # missing_v_bapt_sparse = setdiff(vrbs_baptisms_sparse, names(dat))
-    # missing_v_bapt_full = setdiff(vrbs_baptisms_full, names(dat))
-    # missing_v_opg_full = setdiff(vrbs_opgaafrol_full, names(dat))
 
     cat("Missing for ", modstring, ": \n",vrbs_missing, 
         "\n--------------\n")
-    # cat("Missing for baptisms sparse:", missing_v_bapt_sparse, sep = "\n")
-    # cat("Missing for baptisms full:", missing_v_bapt_full, sep = "\n")
-    # cat("Missing for opgaafrol_full:", missing_v_opg_full, sep = "\n")
 
     vrbs_present = setdiff(vrbs, vrbs_missing)
-    # present_v_sparse = setdiff(vrbs_baptisms_sparse, missing_v_bapt_sparse)
-    # present_v_full = setdiff(vrbs_baptisms_full, missing_v_bapt_full)
 
-    classes = c(
+    expected_classes = c(
         mlast = "character",
         mfirst = "character",
         wlast = "character",
@@ -113,28 +107,36 @@ preflight = function(dat,
         year = "numeric"
     )
 
-    dat_classes = sapply(dat, class)
+    actual_classes = sapply(dat, class)
 
-    # classes_should_be = classes[names(classes) %in% names(dat_classes)] # [match(names(dat_classes), names(classes))]
-    classes_are = dat_classes[names(dat_classes) %in% names(classes)] # [match(names(dat_classes), names(classes))]
+    actual_classes = actual_classes[names(actual_classes) %in% names(expected_classes)]
 
     print(
         data.frame(
-            classes_should_be = classes,
-            classes_are = classes_are[names(classes)]))
+            expected_classes,
+            actual_classes = actual_classes[names(expected_classes)]))
 
     cat("\nShare missing variables (cannot be matched):\n")
-    print(dat[, lapply(.SD, function(x) mean(is.na(x), na.rm = TRUE)), 
+    print(dat[, 
+        lapply(.SD, function(x) mean(is.na(x), na.rm = TRUE)), 
         .SDcols = vrbs_present])
     cat("\n\n")
     
     cat('Share empty variables ("") (matching may fail):\n')
-    print(dat[, lapply(.SD, function(x) mean(nchar(x) == 0, na.rm = TRUE)), 
+    print(dat[, 
+        lapply(.SD, function(x) mean(nchar(x) == 0, na.rm = TRUE)), 
         .SDcols = vrbs_present])
     cat("\n\n")
     
     cat('Share length one string variables ("*") (matching may fail):\n')
-    print(dat[, lapply(.SD, function(x) mean(nchar(x) == 1, na.rm = TRUE)), 
+    print(dat[, 
+        lapply(.SD, function(x) mean(nchar(x) == 1, na.rm = TRUE)), 
+        .SDcols = vrbs_present])
+    cat("\n\n")
+
+    cat("Share multiword string variables (models expect joint middle names or joint prefixes)")
+    print(dat[, 
+        lapply(.SD, function(x) mean(stringi::stri_count_words(x) > 1)),
         .SDcols = vrbs_present])
     cat("\n\n")
 
@@ -145,19 +147,23 @@ preflight = function(dat,
         .SDcols = vrbs_present])
     cat("\n\n")
 
-    # this seems a bit broken: why not do toupper(x) != x & tolower on the one above?
     cat('Share UPPER CASE (matching requires consistency in case between datasets):\n')
-    print(dat[, lapply(.SD, function(x) mean(grepl("^[A-Z ]+$", x), na.rm = TRUE)), 
+    print(dat[, lapply(.SD, function(x) mean(stringi::stri_trans_toupper(x) == x, na.rm = TRUE)), 
         .SDcols = vrbs_present])
     cat("\n\n")
 
     cat('Share lower case (matching requires consistency in case between datasets):\n')
-    print(dat[, lapply(.SD, function(x) mean(grepl("^[a-z ]+$", x), na.rm = TRUE)), 
+    print(dat[, lapply(.SD, function(x) mean(stringi::stri_trans_tolower(x) == x & x != "", na.rm = TRUE)), 
         .SDcols = vrbs_present])
     cat("\n\n")
 
-    cat('Share accented letters and non-alphabetic or symbols per variable (matching might require consistency):\n')
-    print(dat[, lapply(.SD, function(x) mean(grepl("[^A-z ]", x))), .SDcols = vrbs_present])
+    cat('Share accented letters and non-alphabetic symbols per variable (matching might require consistency):\n')
+    print(dat[, 
+        lapply(.SD, function(x) mean(grepl("[^A-z ]", x))), 
+        .SDcols = vrbs_present])
+    cat("\n\n")
+    
+    cat('Accented letters and non-alphabetic symbols\n')
     print(
         lapply(dat[, .SD, .SDcols = vrbs_present], function(column){
             unique(c(stringi::stri_extract_all_regex(column, "[^A-z ]", simplify = TRUE, omit_no_match = TRUE)))
@@ -166,27 +172,12 @@ preflight = function(dat,
     cat("\n\n")
 
     cat('Range (important for numeric variables):\n')
-    print(dat[, lapply(.SD, range, na.rm = TRUE), .SDcols = vrbs_present])
+    print(dat[, 
+        lapply(.SD, range, na.rm = TRUE), 
+        .SDcols = vrbs_present])
     cat("\n\n")
 
-    # mfirst has middle names?
 
 
-    # dataset names, both numeric and character
-    # from found names to 
-        # enough names
-        # printable ascii
-            # accented characters removal?
-        # missing values
-            # number of observations as share of dataset
-            # too many observations
-        # upper/lower case
         # mfirst has middle names?
-        # actually numeric
-            # range of numeric
-
-    # and check rest of data cleaning
 }
-
-# lapply(pretrained_models, function(x) x$variables)
-# lapply(pretrained_models, `[`, "variables")
